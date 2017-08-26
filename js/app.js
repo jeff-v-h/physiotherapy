bfed07fe43fd6fbab36fcdb654a4e08544b03459
jeffvhuang <jh0791@gmail.com>
2017-08-27 09:00:10 +1000

    Add canvas and draw events

--- js/app.js
+++ js/app.js
@@ -28,6 +28,48 @@ $('#dob').focusout(function() {
 	console.log(dob);
 });
 
+// Add body chart image to canvas
+function loadCanvas() { //getContext below only works on DOM element, not jQuery object. Hence getElementById is used
+	var canvas = document.getElementById("myCanvas"); 
+	var context = canvas.getContext('2d');
+	
+	var img = new Image();
+	var x = 0;
+	var y = -10;
+	var imgWidth = canvas.width;
+	var imgHeight = canvas.height;
+
+	img.onload = function() {
+		context.drawImage(img, x, y, imgWidth, imgHeight);
+	}
+	img.src = "img/body_chart_0.5x.png";
+}
+loadCanvas();
+
+// Create drawing capabilities onto canvas
+var colour = 'red';
+var $canvas = $('#myCanvas');
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+
+$canvas.mousedown(function(event) {
+	lastEvent = event;
+	mouseDown = true;
+}).mousemove(function(event) {
+	// Draw lines
+	if (mouseDown) {
+		context.beginPath();
+		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		context.lineTo(event.offsetX, event.offsetY);
+		context.strokeStyle = colour;
+		context.stroke();
+		lastEvent = event;
+	}
+}).mouseup(function() {
+	mouseDown = false;
+});
+
 
 // Add inputs when buttons are clicked
 var impressionForm = $('#impression').find('form');
