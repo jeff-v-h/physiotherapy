55902ef5ee52be6c80e6617aa841b6d8e81cb5f3
jeffvhuang <jh0791@gmail.com>
2017-08-31 22:39:38 +1000

    Add colour palette and clear function

--- js/app.js
+++ js/app.js
@@ -29,27 +29,28 @@ $('#dob').focusout(function() {
 });
 
 // Add body chart image to canvas
-function loadCanvas() { //getContext below only works on DOM element, not jQuery object. Hence getElementById is used
-	var canvas = document.getElementById("myCanvas"); 
-	var context = canvas.getContext('2d');
-	
+// getContext() below only works on DOM element, not jQuery object. Hence getElementById is used
+var canvas = document.getElementById("myCanvas"); 
+var context = canvas.getContext('2d');
+
+function loadCanvas() { 
 	var img = new Image();
 	var x = 0;
-	var y = -10;
+	var y = -20;
 	var imgWidth = canvas.width;
 	var imgHeight = canvas.height;
 
 	img.onload = function() {
 		context.drawImage(img, x, y, imgWidth, imgHeight);
 	}
-	img.src = "img/body_chart_0.5x.png";
+	img.src = "img/body_chart.png";
 }
 loadCanvas();
 
 // Create drawing capabilities onto canvas
-var colour = 'red';
+var colour = $('.selected').css('background-color');
 var $canvas = $('#myCanvas');
-var context = $canvas[0].getContext("2d");
+var $context = $canvas[0].getContext("2d");
 var lastEvent;
 var mouseDown = false;
 
@@ -59,16 +60,29 @@ $canvas.mousedown(function(event) {
 }).mousemove(function(event) {
 	// Draw lines
 	if (mouseDown) {
-		context.beginPath();
-		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-		context.lineTo(event.offsetX, event.offsetY);
-		context.strokeStyle = colour;
-		context.stroke();
+		$context.beginPath();
+		$context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		$context.lineTo(event.offsetX, event.offsetY);
+		$context.strokeStyle = colour;
+		$context.stroke();
 		lastEvent = event;
 	}
 }).mouseup(function() {
 	mouseDown = false;
 });
+
+// Selection of colour
+$('#colours').on('click', 'div', function() {
+	$(this).siblings().removeClass('selected');
+	$(this).addClass('selected');
+	colour = $(this).css('background-color');
+});
+
+// Clear canvas function for button
+function clearCanvas() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	loadCanvas();
+}
 
 
 // Add inputs when buttons are clicked
